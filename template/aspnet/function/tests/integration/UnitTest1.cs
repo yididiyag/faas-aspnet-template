57482b81e79a96c7978950775c48541847cf3159
Yared D. Dessalk <yared.dejene@oneacrefund.org>
2022-09-16 15:52:19 +0200

    Restructure Template to Accommodate Unit and Integration Tests

---
+++ template/aspnet/function/tests/integration/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ template/aspnet/function/tests/unit/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace unit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
